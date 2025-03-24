5623854009abe32e8c70f04f51e39294d66da0cf
RiqueISantos <riquesantosoliveira@gmail.com>
2025-03-24 18:58:53 -0300

    Adicionando html de cadastro e seus estilos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,33 +1,36 @@
-const url = "https://go-wash-api.onrender.com/api/user";
-async function cadastroUsuario(){
-    let name = document.getElementById('name').value
-    let email = document.getElementById('email').value
-    let userType = 1
-    let password = document.getElementById("password").value
-    let cpfCnpj = document.getElementById("cpf_cnpj").value
-    let termos = document.getElementById("termos").checked
-    let birthday = document.getElementById("birthday").value
+async function cadastroUsuario() {
+    let nome = "Alou";
+    let email = "lier2504@uorak.com"  
+    let userType = 1;
+    let password = "1234";
+    let cpfCnpj = "12345678912";
+    let termos = 1;
+    let birthday = "2025-04-03"
 
-    let api = await fetch(url,{
-        method:"POST",
-        body:JSON.stringify({
-            "name":name,
-            "email":email,
-            "user_type_id":userType,
-            "password": password,
-            "cpf_cnpj": cpfCnpj,
-            "terms": termos,
-            "birthday": birthday    
-        }),
-        headers:{
-            'Content-Type':'application/json'
-        }
+    let dados = {
+        "name": nome,
+        "email": email,
+        "user_type_id": userType,
+        "password": password,
+        "cpf_cnpj": cpfCnpj,
+        "terms": termos,
+        "birthday": birthday
+    }
+
+    let api = await fetch("https://go-wash-api.onrender.com/api/user",{
+        method: "POST",
+        body:JSON.stringify(dados),
+        headers: {
+            'Content-Type': 'application/json'
+        } 
     });
 
-    if(api.ok){
-        let resposta = await api.json();
-        console.log(resposta)
+    if (api.ok){
+        let response =  await api.json();
+        console.log(response);
         return
     }
-    let respostaErro = await api.json();
-        console.log(respostaErro.data.errors.cpf_cnpj)}
+
+    let responseErro =  await api.json()
+    console.log(responseErro);
+}
