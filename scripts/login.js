67707ddd982db49885137b7fb7d735cbdfc33da5
RiqueISantos <riquesantosoliveira@gmail.com>
2025-04-20 21:57:14 -0300

    Consumindo a api de login

---
+++ scripts/login.js
@@ -0,0 +1,39 @@
+async function login(){
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let userType = 1;
+
+    let user = {
+        email: email,
+        password: password,
+        user_type_id: userType
+    }
+
+    let url = "https://go-wash-api.onrender.com/api/login";
+
+    let response =  await fetch(url,{
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json'
+        } 
+    })
+
+    let data = await response.json();
+
+    if (response.ok){
+        localStorage.setItem('token', data.access_token);
+        localStorage.setItem('user', JSON.stringify(data.user));
+        console.log(data.user)
+    
+        alert("Login realizado com sucesso!");
+        window.location.href = "/html/compra.html";
+    }else if(response.status === 401){
+        alert("Usuário não está ativo!")
+        console.log(response)
+
+    }else if(response.status === 404){
+        alert("Usuário não foi encontrado!")
+    }
+
+}
