9f137e0478607809ec51699022aec36e513a522a
RiqueISantos <riquesantosoliveira@gmail.com>
2025-05-09 22:18:46 -0300

    criando a sessão de Perfil para dados do usuário

---
+++ scripts/perfil.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+    let name = document.getElementById("name-user");
+    let email = document.getElementById("email-user");
+    let endereco = document.getElementById("endereco-user");
+
+    let userData = JSON.parse(localStorage.getItem("user")) || {};
+
+    name.value = userData.name || "";
+    email.value = userData.email || "";
+    endereco.value = userData.endereco || "";
+
+    
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const editBtn = document.querySelectorAll(".edit-name");
+
+    editBtn.forEach(btn => {
+        btn.addEventListener("click", () =>{
+            const targetId = btn.getAttribute("data-target");
+            const inputField = document.getElementById(targetId);
+
+            if (inputField) {
+                inputField.disabled = !inputField.disabled;
+
+                if(!inputField.disabled){
+                    inputField.focus();
+                }
+            }
+                
+        })
+    });
+
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    lottie.loadAnimation({
+        container: document.getElementById("photo-user"), 
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        path: "https://lottie.host/35aeb85c-1bb5-4a65-a7d8-4e5d0d977052/pJtejRLLwC.json" 
+    });
+});
