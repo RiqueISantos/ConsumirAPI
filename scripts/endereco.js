10421985b40878cc3be56e0b43c1b66af8782f2e
RiqueISantos <riquesantosoliveira@gmail.com>
2025-05-11 21:48:51 -0300

    Adicionando função de cadastro de endereço

---
+++ scripts/endereco.js
@@ -0,0 +1,37 @@
+async function cadastrarEndereco(){
+    let titulo = document.getElementById('titulo').value;
+    let cep = document.getElementById('cep').value;
+    let endereco = document.getElementById('endereco').value;
+    let numero = document.getElementById('numero').value;
+    let complemento = document.getElementById('complemento').value;
+
+    let address = {
+        "title": titulo, 
+        "cep": cep, 
+        "address": endereco, 
+        "number": numero, 
+        "complement": complemento, 
+    }
+    
+    let response = await fetch('https://go-wash-api.onrender.com/api/auth/address', {
+        method: "POST",
+        body:JSON.stringify(address),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        },
+      
+    });
+
+    let data = await response.json();
+
+    if(response.ok){
+        alert("O endereço foi cadastrado com sucesso!");
+        localStorage.setItem('address', JSON.stringify(data));
+        localStorage.removeItem('addresses');
+        console.log("Endereços cadastrados: ", data)
+    }else{
+        alert("erro")
+    }
+
+}
